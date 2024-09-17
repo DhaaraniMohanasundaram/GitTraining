23e0afca529975ad7f275b31c24bce4ea5585258
mohanasundaramdh <Dhaarani.Mohanasundram@trumpf.com>
2024-09-17 09:45:00 +0530

    Adding files

---
+++ Training24/Program.c
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// -------------------------------------
+//  Dhaarani Mohanasundaram
+// -------------------------------------
+// Standard Program file in Main
+// -------------------------------------
+// Program.c
+// Program on main branch.
+// ------------------------------------------------------------------------------------
+
+const char* GetMessage() {
+return "Hello, World!";
+}
---
+++ Training24/Test.c
@@ -0,0 +1,19 @@
+// ------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// -------------------------------------
+//  Dhaarani Mohanasundaram
+// -------------------------------------
+// Standard Test file in Main
+// -------------------------------------
+// Test.c
+// Program on main branch.
+// ------------------------------------------------------------------------------------
+#include <stdio.h>
+#include "Header.h"
+
+int main() {
+const char* message = GetMessage ();
+printf ("%s\n", message);
+return;
+}
