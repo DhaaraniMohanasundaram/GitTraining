5588981014403db9b9dffa290a953f927e8e09a4
mohanasundaramdh <Dhaarani.Mohanasundram@trumpf.com>
2024-09-17 09:38:17 +0530

    Adding files

---
+++ Training24/Header.h
@@ -0,0 +1,17 @@
+// ------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// -------------------------------------
+//  Dhaarani Mohanasundaram
+// -------------------------------------
+// Standard Header file in Main
+// -------------------------------------
+// Header.h
+// Program on main branch.
+// ------------------------------------------------------------------------------------
+#pragma once
+#define HEADER_H
+
+/// <summary>Function to get the "Hello, World!" message</summary>
+///<returns>Pointer to a string containing the "Hello, World!" message</returns>
+const char* GetMessage();
